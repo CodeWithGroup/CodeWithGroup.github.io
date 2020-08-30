c848f71fd051673a90c805f85ce9385dcf2977ed
Drew <dangerworm@gmail.com>
2020-08-30 12:25:33 +0100

    Add new_site folder for committee review

---
+++ _site/js/check-width.js
@@ -0,0 +1,23 @@
+"use strict"
+
+$(function () {
+    var $window = $(window);
+    var $pane = $('#main-container');
+
+    function checkWidth() {
+        var windowsize = $window.width();
+        if (windowsize <= 768) {
+            if ($pane.hasClass('toggled')) {
+                $pane.toggleClass('toggled');
+            }
+        }
+        else if (!$pane.hasClass('toggled')) {
+            $pane.addClass('toggled');
+        }
+    }
+
+    // Execute on load
+    checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth);
+});
---
+++ new_site/js/change-font.js
@@ -0,0 +1,7 @@
+"use strict"
+
+$(function() {
+    $('#change-font').click(function () {
+        $('body').toggleClass('open-dyslexic');
+    });
+})
